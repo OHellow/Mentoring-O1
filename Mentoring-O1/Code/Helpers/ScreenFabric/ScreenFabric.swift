e961db18a52028b98c5f3d45975487cc75b12894
o.satishur <olegsatol@gmail.com>
2023-03-10 15:48:51 +0300

    KR-1-2-Clean-Swift Fix alert showing in Upcoming movies

--- Mentoring-O1/Code/Helpers/ScreenFabric/ScreenFabric.swift
+++ Mentoring-O1/Code/Helpers/ScreenFabric/ScreenFabric.swift
@@ -14,6 +14,7 @@ class ScreenFabric {
         presenter.viewController = viewController
         viewController.interactor = interactor
         viewController.router = router
+        router.viewController = viewController
         return viewController
     }
 
